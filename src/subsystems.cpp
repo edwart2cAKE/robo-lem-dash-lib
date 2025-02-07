b0476d84b57493e0caa70c434d1716d1df51b74c
edwart2cAKE <annamsrihaas@gmail.com>
2025-02-06 21:54:28 -0500

    pre robot testing

--- src/subsystems.cpp
+++ src/subsystems.cpp
@@ -1,30 +1,46 @@
 // ports and subsystems are defined here
 #include "lemlib/chassis/trackingWheel.hpp"
+#include "lemlib/pid.hpp"
 #include "main.h" // IWYU pragma: keep
+#include "pros/motor_group.hpp"
 
 #if 1
 #include "scaledIMU.cpp"
+#include "lift.cpp"
 #else
 #include "scaledIMU.h"
+#include "lift.h"
 #endif
 
 // port numbers
+
+// drivetrain ports
 #define left_drive_ports                                                       \
   { -5, -6, -19 }
 #define right_drive_ports                                                      \
   { 13, 14, 15 }
+
+// intkae ports
 #define intake_port -12
-#define lift_ports {20} // only one motor
+
+// lift + rot sensor ports
+#define lift_ports                                                             \
+  { 20 } // only one motor
+#define rot_sensor_port 10
+
+// mogo port (port C)
 #define mogo_port 3
+
+// imu port
 #define imu_port 7
 
 pros::MotorGroup left_side(left_drive_ports);   // left motors on ports 1, 2, 3
 pros::MotorGroup right_side(right_drive_ports); // right motors on ports 4, 5, 6
 
 // drivetrain settings
-lemlib::Drivetrain drivetrain(&left_side,                 // left motor group
-                              &right_side,                // right motor group
-                              11.25,                      // 11.25 inch track width
+lemlib::Drivetrain drivetrain(&left_side,  // left motor group
+                              &right_side, // right motor group
+                              11.25,       // 11.25 inch track width
                               lemlib::Omniwheel::OLD_325, // using 3.25 omnis
                               450, // drivetrain rpm is 450
                               2    // horizontal drift is 2 (for now)
@@ -80,7 +96,12 @@ lemlib::Chassis chassis(drivetrain, lateral_controller, angular_controller,
 pros::Motor intake(intake_port, pros::v5::MotorGears::green,
                    pros::v5::MotorEncoderUnits::degrees); // intake motor
 
+// lady brown
+lemlib::PID liftPID(5, 0, 0);
 
+pros::MotorGroup lift_motors(lift_ports); // lift motor group
+pros::Rotation lift_sensor(rot_sensor_port);
+Lift lift(lift_motors, lift_sensor, liftPID);
 
 // mogo
 pros::adi::DigitalOut mogo(mogo_port, LOW);
